55a90f9c2bc342b98f270e43dea8573b718fb257
niraj1998shetty <87310954+niraj1998shetty@users.noreply.github.com>
2025-03-13 10:38:44 +0530

    Update AuthContext.jsx

--- kfl-client/src/contexts/AuthContext.jsx
+++ kfl-client/src/contexts/AuthContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
 
 const AuthContext = createContext();
 
