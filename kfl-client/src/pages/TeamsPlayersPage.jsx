57704b8fb89d8747dacb65cc7452b750f74aef76
niraj1998shetty <87310954+niraj1998shetty@users.noreply.github.com>
2026-02-02 21:28:37 +0530

    Update TeamsPlayersPage.jsx

--- kfl-client/src/pages/TeamsPlayersPage.jsx
+++ kfl-client/src/pages/TeamsPlayersPage.jsx
@@ -270,7 +270,7 @@ const TeamsPlayersPage = () => {
                   </p>
                 </div>
               ) : (
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                <div className="grid grid-cols-2 lg:grid-cols-3 gap-3 sm:gap-6">
                   {teams.map((team) => (
                     <TeamCard
                       key={team.code}
